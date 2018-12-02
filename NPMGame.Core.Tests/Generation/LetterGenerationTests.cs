dc79c68ae9e7b45d698f8dcd8192aea95ebd547f
cypherix93 <bikram.ghosh93@gmail.com>
2018-12-02 01:11:45 -0500

    Added tests to test the generation

---
+++ NPMGame.Core.Tests/Generation/LetterGenerationTests.cs
@@ -0,0 +1,35 @@
+﻿using NPMGame.Core.Letters;
+using NPMGame.Core.Models;
+using NUnit.Framework;
+
+namespace NPMGame.Core.Tests.Generation
+{
+    [TestFixture]
+    public class LetterGenerationTests
+    {
+        [OneTimeSetUp]
+        public void RunBeforeAllTests()
+        {
+            LettersCollection.Init();
+        }
+
+        [Test]
+        public void TestLetterGeneration()
+        {
+            var generated = LettersGenerator.GenerateLetter();
+
+            Assert.That(generated, Is.Not.Null);
+            Assert.That(generated, Is.TypeOf<Letter>());
+
+            Assert.That(generated.Code, Is.Not.Zero);
+            Assert.That(generated.Score, Is.Not.Zero);
+            Assert.That(generated.OccurrenceCount, Is.Not.Zero);
+        }
+
+        [Test]
+        public void TestLetterGenerationProbabilities()
+        {
+
+        }
+    }
+}
