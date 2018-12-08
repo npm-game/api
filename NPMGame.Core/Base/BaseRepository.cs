cad45ce68944df712e9a0e403b9980bc45c6298d
cypherix93 <bikram.ghosh93@gmail.com>
2018-12-08 00:10:08 -0500

    Some base classes

---
+++ NPMGame.Core/Base/BaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using Marten;
+
+namespace NPMGame.Core.Base
+{
+    public class BaseRepository
+    {
+        protected readonly IDocumentStore _store;
+
+        public BaseRepository(IDocumentStore documentStore)
+        {
+            _store = documentStore;
+        }
+    }
+}
---
+++ NPMGame.Core/Base/BaseService.cs
@@ -0,0 +1,14 @@
+﻿using NPMGame.Core.Services;
+
+namespace NPMGame.Core.Base
+{
+    public class BaseService
+    {
+        protected readonly IUnitOfWork UnitOfWork;
+
+        protected BaseService(IUnitOfWork unitOfWork)
+        {
+            UnitOfWork = unitOfWork;
+        }
+    }
+}
