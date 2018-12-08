63011a907814019498d3730ed51f786b7b49ec36
cypherix93 <bikram.ghosh93@gmail.com>
2018-12-08 00:10:14 -0500

    Added game session model

---
+++ NPMGame.Core/Models/Game/GameSession.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Marten.Schema;
+using NPMGame.Core.Models.Identity;
+
+namespace NPMGame.Core.Models.Game
+{
+    public class GameSession
+    {
+        public Guid Id { get; set; }
+
+        public GameState State { get; set; }
+        public GameOptions Options { get; set; }
+
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public List<GamePlayer> Players { get; set; }
+
+        [ForeignKey(typeof(GamePlayer))]
+        public Guid CurrentTurnPlayerId { get; set; }
+
+        public GameSession(GameOptions options)
+        {
+            State = GameState.NotStarted;
+            Options = options;
+        }
+    }
+
+    public enum GameState
+    {
+        NotStarted,
+        Setup,
+        InProgress,
+        Done
+    }
+
+    public class GamePlayer
+    {
+        [ForeignKey(typeof(User))]
+        public Guid UserId { get; set; }
+
+        public List<Letter> Hand { get; set; }
+
+        public int Score { get; set; }
+        public double Multiplier { get; set; }
+    }
+
+    public class GameOptions
+    {
+        public int Goal { get; set; }
+        public int HandSize { get; set; }
+    }
+}
