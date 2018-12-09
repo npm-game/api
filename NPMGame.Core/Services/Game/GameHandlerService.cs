a74a3cdcf63e61b2a1f34aae892e9785316f52d2
cypherix93 <bikram.ghosh93@gmail.com>
2018-12-08 20:36:08 -0500

    Broke out the game master with a new game handler that handles a specific game

---
+++ NPMGame.Core/Services/Game/GameHandlerService.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using NPMGame.Core.Base;
+using NPMGame.Core.Constants.Localization;
+using NPMGame.Core.Models.Enums;
+using NPMGame.Core.Models.Exceptions;
+using NPMGame.Core.Models.Game;
+using NPMGame.Core.Repositories.Game;
+using NPMGame.Core.Repositories.Identity;
+using NPMGame.Core.Workers.Letters;
+using NPMGame.Core.Workers.Words;
+
+namespace NPMGame.Core.Services.Game
+{
+    public class GameHandlerService : BaseService
+    {
+        public GameSession Game { get; private set; }
+
+        public GameHandlerService(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+
+        public GameHandlerService UsingGame(GameSession game)
+        {
+            Game = game;
+
+            return this;
+        }
+
+        public async Task<GameSession> AddPlayerToGame(Guid userId)
+        {
+            // TODO: Check if player already part of any other game
+            var user = await UnitOfWork.GetRepository<UserRepository>().Get(userId);
+
+            if (user == null)
+            {
+                throw new GameException(ErrorMessages.UserNotFound);
+            }
+
+            // TODO: Create GamePlayer factory to handle user ties and all that
+            var player = new GamePlayer(user.Id);
+
+            Game.Players.Add(player);
+
+            return await SaveGame();
+        }
+
+        public async Task<GameSession> StartGame()
+        {
+            Game.State = GameState.InProgress;
+            Game.StartTime = DateTime.Now;
+
+            // TODO: Do a dice roll to pick the first player
+            Game.CurrentTurnPlayerId = Game.Players.First().UserId;
+
+            // Fill each player's hand
+            foreach (var player in Game.Players)
+            {
+                await FillPlayerHand(player);
+            }
+
+            return await SaveGame();
+        }
+
+        public async Task<GameSession> TakeTurn(GameTurnAction turnAction)
+        {
+            var currentPlayer = Game.Players.FirstOrDefault(p => p.UserId == turnAction.PlayerId);
+
+            if (currentPlayer == null)
+            {
+                throw new GameException(ErrorMessages.PlayerNotInGame);
+            }
+
+            if (Game.CurrentTurnPlayerId != turnAction.PlayerId)
+            {
+                throw new GameException(ErrorMessages.NotYourTurn);
+            }
+
+            if (turnAction is GameTurnGuessAction guessAction)
+            {
+                await ProcessGuessTurn(currentPlayer, guessAction);
+            }
+            if (turnAction is GameTurnSwitchAction switchAction)
+            {
+                await ProcessSwitchTurn(currentPlayer, switchAction);
+            }
+
+            return await SaveGame();
+        }
+
+        private async Task<GameSession> SaveGame()
+        {
+            return await UnitOfWork.GetRepository<GameSessionRepository>().Update(Game);
+        }
+
+        private async Task ProcessGuessTurn(GamePlayer currentPlayer, GameTurnGuessAction turnAction)
+        {
+            var matchType = await WordMatcher.MatchWordAgainstNPM(turnAction.WordGuessed);
+
+            if (matchType == MatchType.Partial)
+            {
+                if (currentPlayer.Streak > 0)
+                {
+                    currentPlayer.Streak = 0;
+                }
+            }
+            else
+            {
+                var wordScore = WordScorer.GetScoreForWord(turnAction.WordGuessed);
+
+                if (matchType == MatchType.Exact)
+                {
+                    if (currentPlayer.Streak > 0)
+                    {
+                        currentPlayer.Streak = 0;
+                    }
+                    else
+                    {
+                        currentPlayer.Streak--;
+                    }
+
+                    currentPlayer.Score -= Math.Min(currentPlayer.Score, wordScore);
+                }
+                else if (matchType == MatchType.None)
+                {
+                    currentPlayer.Streak++;
+                    currentPlayer.Score += wordScore;
+                }
+            }
+        }
+
+        private async Task ProcessSwitchTurn(GamePlayer currentPlayer, GameTurnSwitchAction turnAction)
+        {
+            foreach (var character in turnAction.CharactersSwitched)
+            {
+                var letterToRemove = currentPlayer.Hand.First(l => l.Code == character);
+                currentPlayer.Hand.Remove(letterToRemove);
+            }
+
+            await FillPlayerHand(currentPlayer);
+        }
+
+        private async Task FillPlayerHand(GamePlayer player)
+        {
+            var lettersInHandCount = player.Hand.Count;
+            var lettersToFill = Game.Options.HandSize - lettersInHandCount;
+
+            var generatedLetters = await LettersGenerator.GenerateLetters(lettersToFill);
+
+            player.Hand.AddRange(generatedLetters);
+        }
+    }
+}
--- NPMGame.Core/Services/Game/GameMasterService.cs
+++ NPMGame.Core/Services/Game/GameMasterService.cs
@@ -1,35 +1,43 @@
 ﻿using System;
-using System.Linq;
 using System.Threading.Tasks;
-using Marten;
 using NPMGame.Core.Base;
 using NPMGame.Core.Constants.Localization;
 using NPMGame.Core.Models.Exceptions;
 using NPMGame.Core.Models.Game;
 using NPMGame.Core.Repositories.Game;
-using NPMGame.Core.Repositories.Identity;
 
 namespace NPMGame.Core.Services.Game
 {
     public class GameMasterService : BaseService
     {
-        public GameMasterService(IUnitOfWork unitOfWork) : base(unitOfWork)
+        private readonly GameHandlerService _handler;
+
+        public GameMasterService(GameHandlerService handler, IUnitOfWork unitOfWork) : base(unitOfWork)
         {
+            _handler = handler;
         }
 
         public async Task<GameSession> CreateGame(Guid creatorUserId, GameOptions options = null)
         {
-            // TODO: Create GameSession factory to handle creator and default options and all that
+            if (options == null)
+            {
+                options = new GameOptions
+                {
+                    Goal = 100,
+                    HandSize = 7
+                };
+            }
+
             var game = new GameSession(options);
 
-            var createdGame = await UnitOfWork.GetRepository<GameSessionRepository>().Create(game);
+            game = await UnitOfWork.GetRepository<GameSessionRepository>().Create(game);
 
-            await AddPlayerToGame(createdGame.Id, creatorUserId);
+            game = await _handler.UsingGame(game).AddPlayerToGame(creatorUserId);
 
-            return createdGame;
+            return game;
         }
 
-        public async Task<GameSession> AddPlayerToGame(Guid gameId, Guid userId)
+        public async Task<GameHandlerService> ForGame(Guid gameId)
         {
             var game = await UnitOfWork.GetRepository<GameSessionRepository>().Get(gameId);
 
@@ -38,42 +46,7 @@ namespace NPMGame.Core.Services.Game
                 throw new GameException(ErrorMessages.GameNotFound);
             }
 
-            // TODO: Check if player already part of any other game
-            var user = await UnitOfWork.GetRepository<UserRepository>().Get(userId);
-
-            if (user == null)
-            {
-                throw new GameException(ErrorMessages.UserNotFound);
-            }
-
-            // TODO: Create GamePlayer factory to handle user ties and all that
-            var player = new GamePlayer(user.Id);
-
-            game.Players.Add(player);
-
-            var updatedGame = await UnitOfWork.GetRepository<GameSessionRepository>().Update(game);
-
-            return updatedGame;
-        }
-
-        public async Task<GameSession> StartGame(Guid gameId)
-        {
-            var game = await UnitOfWork.GetRepository<GameSessionRepository>().Get(gameId);
-
-            if (game == null)
-            {
-                throw new GameException(ErrorMessages.GameNotFound);
-            }
-
-            game.State = GameState.InProgress;
-            game.StartTime = DateTime.Now;
-
-            // TODO: Do a dice roll to pick the first player
-            game.CurrentTurnPlayerId = game.Players.First().UserId;
-
-            var updatedGame = await UnitOfWork.GetRepository<GameSessionRepository>().Update(game);
-
-            return updatedGame;
+            return _handler.UsingGame(game);
         }
     }
 }
